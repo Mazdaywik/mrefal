58b565b18a95c7b975875fc77e84d0dedac2a8ed
Маздайщик <a.v.konovalov87@mail.ru>
2007-10-07 00:49:00 +0300

    Commit created from recovered archive 2007-10-07_00-49-00 1-105685 083707566-differencial.rar

---
+++ MR-Home/update-library.bat
@@ -0,0 +1,6 @@
+rem @echo off
+setlocal
+	set libraries=FileIO InOut Math MOrder MOS MSelfDiag MStrings Types
+	for %%s in ( %libraries% ) do copy ..\Defs\%%s.rsym Defs\%%s.rsym
+	for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout2 ROut\%%o.rout2
+endlocal

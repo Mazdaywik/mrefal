03ab25e9781a1add8c1adc1a83be727a40ae1b7f
Маздайщик <a.v.konovalov87@mail.ru>
2007-10-20 19:30:00 +0300

    Commit created from recovered archive 2007-10-20_19-30-00 1-105683 083706990-full.rar

--- MR-Home/update-library.bat
+++ MR-Home/update-library.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-	set libraries=FileIO InOut Math MOrder MOS MSelfDiag MStrings Types
+	set libraries=FileIO InOut Math MOrder MOS MSelfDiag MStrings Types MFileSystem
 	for %%s in ( %libraries% ) do copy ..\Defs\%%s.rsym Defs\%%s.rsym
-	for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout2 ROut\%%o.rout2
+	rem for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout2 ROut\%%o.rout2
 	for %%o in ( %libraries% ) do copy ..\ROut2\%%o.rout2 ROut2\%%o.rout2
 endlocal

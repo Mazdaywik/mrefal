8608ccdbf57a4244cac883015c7411fb411f9418
Маздайщик <a.v.konovalov87@mail.ru>
2008-02-21 15:36:00 +0300

    Commit created from recovered archive 2008-02-21_15-36-00 1-105575 083655118-differencial.rar

--- MR-Home/update-library.bat
+++ MR-Home/update-library.bat
@@ -3,7 +3,7 @@ setlocal
 	set libraries=FileIO InOut Math MOrder MOS MSelfDiag MStrings Types MFileSystem
 	set libraries=%libraries% MDosWinRecoder
 	for %%s in ( %libraries% ) do copy ..\Defs\%%s.rsym Defs\%%s.rsym
-	rem for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout2 ROut\%%o.rout2
+	for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout ROut\%%o.rout
 	for %%o in ( %libraries% ) do copy ..\ROut2\%%o.rout2 ROut2\%%o.rout2
 	for %%o in ( %libraries% ) do copy ..\ROut3\%%o.rout3 ROut3\%%o.rout3
 endlocal

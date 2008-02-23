081cdb6ea0d5009594446ccf43d133fa41114779
Маздайщик <a.v.konovalov87@mail.ru>
2008-02-23 23:40:00 +0300

    Commit created from recovered archive 2008-02-23_23-40-00 1-105577 083655710-differencial.rar

--- Utilities/make_all.bat
+++ Utilities/make_all.bat
@@ -1,5 +1,6 @@
 @echo off
 set PROGRAMS=grab_info text-to-html TODO_list error_parser Recoder VersionUpdater
+set PROGRAMS=%PROGRAMS% MergeFindResults
 
 refgo VersionUpdater /verfile:Utilities_version.txt /srcfile:mUtilitiesBanner.mref >nul
 
@@ -7,9 +8,10 @@ if exist compilation.log del compilation.log
 
 for %%p in ( %PROGRAMS% ) do (
 	echo Compiling %%p ...
+	echo.>> compilation.log
 	call compile_mr %%p.mref >> compilation.log
 	echo.
 )
 
 del *.ref
-rd /s /q Defs Info ROut3
+rd /s /q Defs Info ROut

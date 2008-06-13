40b8d2c6e5375d97f8ba9e3b0bba4cf312884cf6
Маздайщик <a.v.konovalov87@mail.ru>
2008-06-13 18:17:00 +0300

    Commit created from recovered archive 2008-06-13_18-17-00 1-105594 083660950-differencial.rar

--- Info/unlib.bat
+++ Info/unlib.bat
@@ -1,1 +1,1 @@
-@type "not linked.txt" | find /V "::InOut::" | find /V "::MRefalRTS::" | find /V "::MOS::" | find /V "::FileIO::" | find /V "::MStrings::" | find /V "::MOrder::" | find /V "::MSelfDiag::" | find /V "::MFileSystem::" | find /V "::Math::" > not" "linked.txt
+@type "not linked.txt" | find /V "::InOut::" | find /V "::MRefalRTS::" | find /V "::MOS::" | find /V "::FileIO::" | find /V "::MStrings::" | find /V "::MOrder::" | find /V "::MSelfDiag::" | find /V "::MFileSystem::" | find /V "::Math::" | find /V "::MSwapSupport::" > not" "linked.txt
--- MR-Home/update-library.bat
+++ MR-Home/update-library.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 	set libraries=FileIO InOut Math MOrder MOS MSelfDiag MStrings Types MFileSystem
-	set libraries=%libraries% MDosWinRecoder
+	set libraries=%libraries% MDosWinRecoder MSwapSupport
 	for %%s in ( %libraries% ) do copy ..\Defs\%%s.rsym Defs\%%s.rsym
 	for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout ROut\%%o.rout
 	for %%o in ( %libraries% ) do copy ..\ROutS\%%o.routS ROutS\%%o.routS

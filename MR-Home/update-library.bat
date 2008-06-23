2263996c15c776fbaa2abe51a6dcbc25355fe2e1
Маздайщик <a.v.konovalov87@mail.ru>
2008-06-23 22:17:00 +0300

    Commit created from recovered archive 2008-06-23_22-17-00 1-105644 083685582-full.rar

--- MR-Home/update-library.bat
+++ MR-Home/update-library.bat
@@ -4,5 +4,5 @@ setlocal
 	set libraries=%libraries% MDosWinRecoder MSwapSupport
 	for %%s in ( %libraries% ) do copy ..\Defs\%%s.rsym Defs\%%s.rsym
 	for %%o in ( %libraries% ) do copy ..\ROut\%%o.rout ROut\%%o.rout
-	for %%o in ( %libraries% ) do copy ..\ROutS\%%o.routS ROutS\%%o.routS
+	for %%o in ( %libraries% ) do copy ..\ROutS\%%o.srout ROutS\%%o.srout
 endlocal

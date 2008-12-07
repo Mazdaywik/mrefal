7f800672727a53cfabd1c41635ce839c59c5147c
Маздайщик <a.v.konovalov87@mail.ru>
2008-12-07 20:40:00 +0300

    Commit created from recovered archive 2008-12-07_20-40-00 1-105657 083694302-full.rar

--- Utilities/make_all.bat
+++ Utilities/make_all.bat
@@ -4,14 +4,9 @@ set PROGRAMS=%PROGRAMS% MergeFindResults ErrorDecoder
 
 refgo VersionUpdater /verfile:Utilities_version.txt /srcfile:mUtilitiesBanner.mref >nul
 
-if exist compilation.log del compilation.log
+if exist _compilation.log del _compilation.log
 
-for %%p in ( %PROGRAMS% ) do (
-  echo Compiling %%p ...
-  echo.>> compilation.log
-  call compile_mr %%p.mref >> compilation.log
-  echo.
-)
+call compile_mr %PROGRAMS% >> _compilation.log
 
 del *.ref
 rd /s /q Defs Info ROut

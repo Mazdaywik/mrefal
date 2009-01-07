80ba9bbdb7e453fd74a1661a09a91d1149ea8b9b
Маздайщик <a.v.konovalov87@mail.ru>
2009-01-08 02:21:00 +0300

    Commit created from recovered archive 2009-01-08_02-21-00 1-105613 083667358-differencial.rar

--- MR-Home/update-library.bat
+++ MR-Home/update-library.bat
@@ -4,5 +4,6 @@ setlocal
   set libraries=%libraries% MDosWinRecoder MSwapSupport MXXIO MR-Extent
   for %%s in ( %libraries% ) do copy ..\~Defs\%%s.rsym ~Defs\%%s.rsym
   for %%o in ( %libraries% ) do copy ..\~ROut\%%o.rout ~ROut\%%o.rout
+  for %%o in ( %libraries% ) do copy ..\~XLinx\%%o.xlinx ~XLinx\%%o.xlinx
   for %%o in ( %libraries% ) do copy ..\~ROutS\%%o.srout ~ROutS\%%o.srout
 endlocal
--- make_test.bat
+++ make_test.bat
@@ -1,4 +1,4 @@
 erase test.rsl
 rem call compile_mr Test.mref
 refgo -c100 -t mrefal +make+ "=MP:.;MR-Home" Test.mref
-refgo test.rsl
+refgo -t test.rsl

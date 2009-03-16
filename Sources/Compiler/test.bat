453dac2480a46579365562432761921d088b9c7d
Маздайщик <a.v.konovalov87@mail.ru>
2009-03-16 12:24:00 +0300

    Commit created from recovered archive 2009-03-16_12-24-00 1-105415 083226558-full.rar

--- Sources/Compiler/test.bat
+++ Sources/Compiler/test.bat
@@ -1,8 +1,2 @@
 rem @echo off
-set MP=+MP:. +MP:../Libraries/all.all
-set MP=%MP% +MP:../Libraries/all.r5-b
-set MP=%MP% +MP:../Libraries/all.r5-s
-set MP=%MP% +MP:../Libraries/windows.all
-set MP=%MP% +MP:../Libraries/windows.r5-b
-set MP=%MP% +MP:../Libraries/windows.r5-s
-refgo -c150 mrefal.rsl +make +xlinx +be:R5 %MP% MXLinx.mref
+refgo -c150 mrefal.rsl +make +xlinx +be:R5 +cfg:test.cfg MXLinx.mref

b18725de5d76942d012776015b509fbb7a51d8ce
Маздайщик <a.v.konovalov87@mail.ru>
2009-04-16 00:36:00 +0300

    Commit created from recovered archive 2009-04-16_00-36-00 1-105382 080058086-full.rar

--- Sources/Compiler/test.bat
+++ Sources/Compiler/test.bat
@@ -1,2 +1,2 @@
 rem @echo off
-refgo -c150 mrefal.rsl +make +xlinx +be:R5 +cfg:test.cfg MXLinx.mref
+refgo -c150 mrefal.rsl +make +xlinx +be:R5 +cfg:test.cfg +tune-be:Refal5 ( -ext:tuor ) MLambda.mref

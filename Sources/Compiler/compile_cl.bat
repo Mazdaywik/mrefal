66edd853620ea2c49cd66456ef6fc970a26e13cf
Маздайщик <a.v.konovalov87@mail.ru>
2009-05-07 23:36:00 +0300

    Commit created from recovered archive 2009-05-07_23-36-00 1-104082 031911766-differencial.rar

--- Sources/Compiler/compile_cl.bat
+++ Sources/Compiler/compile_cl.bat
@@ -1,4 +1,4 @@
 @set msvc=C:\Program Files\Microsoft Visual Studio 8
 @copy "%msvc%\Common7\IDE\mspdb80.dll" mspdb80.dll
-"%msvc%\VC\bin\"cl.exe  /I"%mcvc%\VC\PlatformSDK\Include" /I"%msvc%\VC\include" /Zc:forScope /wd4996  %*  /EHsc /link /LIBPATH:"%msvc%\VC\lib" /LIBPATH:"%mcvc%\VC\PlatformSDK\lib"
+@"%msvc%\VC\bin\"cl.exe /Os /I"%mcvc%\VC\PlatformSDK\Include" /I"%msvc%\VC\include" /Zc:forScope /wd4996  %*  /EHsc /link /LIBPATH:"%msvc%\VC\lib" /LIBPATH:"%mcvc%\VC\PlatformSDK\lib" >nul
 @erase mspdb80.dll

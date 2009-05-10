337095ec79f90c6bac29d16e296b2641d6ed54b0
Маздайщик <a.v.konovalov87@mail.ru>
2009-05-11 00:07:00 +0300

    Commit created from recovered archive 2009-05-11_00-07-00 1-104066 031710830-full.rar

--- Sources/Compiler/compile_cl.bat
+++ Sources/Compiler/compile_cl.bat
@@ -1,4 +1,4 @@
-@set msvc=D:\Program Files\Microsoft Visual Studio 8
+@set msvc=C:\Program Files\Microsoft Visual Studio 8
 @copy "%msvc%\Common7\IDE\mspdb80.dll" mspdb80.dll
-@"%msvc%\VC\bin\"cl.exe /Os /I"%mcvc%\VC\PlatformSDK\Include" /I"%msvc%\VC\include" /Zc:forScope /wd4996  %*  /EHsc /link /LIBPATH:"%msvc%\VC\lib" /LIBPATH:"%mcvc%\VC\PlatformSDK\lib" >nul
+@"%msvc%\VC\bin\"cl.exe /wd4996 /Os /I"%msvc%\VC\PlatformSDK\Include" /I"%msvc%\VC\include" /Zc:forScope  %*  /EHsc /link /LIBPATH:"%msvc%\VC\lib" /LIBPATH:"%mcvc%\VC\PlatformSDK\lib"
 @erase mspdb80.dll
--- Sources/Compiler/test.bat
+++
@@ -1,2 +0,0 @@
-rem @echo off
-refgo -c150 mrefal.rsl +make +xlinx +be:R5 +cfg:test.cfg +tune-be:Refal5 ( -ext:tuor ) MLambda.mref

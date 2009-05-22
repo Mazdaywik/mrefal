aa05d78383ee678f4d328393f034da8d36d10993
Маздайщик <a.v.konovalov87@mail.ru>
2009-05-22 17:40:00 +0300

    Commit created from recovered archive 2009-05-22_17-40-00 1-104051 025522078-full.rar

--- stable_update.bat
+++ stable_update.bat
@@ -1,11 +1,16 @@
 @echo off
-for %%i in (~Defs ~ROut ~XLinx) do (
+for %%i in (~Defs ~ROut ~XLinx ~SimRef) do (
   xcopy /S /I /Y Sources\Libraries\all.all\%%i      Libraries\all.all\%%i
   xcopy /S /I /Y Sources\Libraries\all.r5-b\%%i     Libraries\all.r5\%%i
   xcopy /S /I /Y Sources\Libraries\all.r5-s\%%i     Libraries\all.r5\%%i
+  xcopy /S /I /Y Sources\Libraries\all.sr-b\%%i     Libraries\all.sr\%%i
+  xcopy /S /I /Y Sources\Libraries\all.sr-s\%%i     Libraries\all.sr\%%i
   xcopy /S /I /Y Sources\Libraries\windows.all\%%i  Libraries\windows.all\%%i
   xcopy /S /I /Y Sources\Libraries\windows.r5-b\%%i Libraries\windows.r5\%%i
   xcopy /S /I /Y Sources\Libraries\windows.r5-s\%%i Libraries\windows.r5\%%i
+  xcopy /S /I /Y Sources\Libraries\windows.sr-b\%%i Libraries\windows.sr\%%i
+  xcopy /S /I /Y Sources\Libraries\windows.sr-s\%%i Libraries\windows.sr\%%i
 )
+copy /Y Sources\Compiler\mrefal.exe Bin\mrefal.exe
 copy /Y Sources\Compiler\mrefal.rsl Bin\mrefal.rsl
 copy /Y Sources\MR-Extent\mr-extent.exe Bin\mr-extent.exe

d0ad45351a23ea326981ddee62127bc42ad12ca9
Маздайщик <a.v.konovalov87@mail.ru>
2009-08-05 11:07:13 +0300

    Commit created from recovered archive MRefal.full.2009-08-05-11-07-13.rar

--- Sources/Compiler/compile_cl.bat
+++
@@ -1,4 +0,0 @@
-@set msvc=C:\Program Files\Microsoft Visual Studio 8
-@copy "%msvc%\Common7\IDE\mspdb80.dll" mspdb80.dll
-@"%msvc%\VC\bin\"cl.exe /wd4996 /Os /I"%msvc%\VC\PlatformSDK\Include" /I"%msvc%\VC\include" /Zc:forScope  %*  /EHsc /link /LIBPATH:"%msvc%\VC\lib" /LIBPATH:"%mcvc%\VC\PlatformSDK\lib"
-@erase mspdb80.dll
--- Sources/Compiler/compile_sr.bat
+++ Sources/Compiler/compile_sr.bat
@@ -1,1 +1,1 @@
-@srefc -c "compile_cl /I\"../../Simple Refal/SRLib\"" -d ..\..\Simple" "Refal\SRLib %* refalrts Library WinLib
+@srefc -c "compile_bcc -I\"../../Simple Refal/SRLib\"" -d ..\..\Simple" "Refal\SRLib %* refalrts Library WinLib

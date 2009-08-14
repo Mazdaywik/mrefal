bb1eab3381a146a34d20a66e9a859f3606a538de
Маздайщик <a.v.konovalov87@mail.ru>
2009-08-15 01:23:08 +0300

    Commit created from recovered archive MRefal.full.2009-08-15-01-23-08.rar

--- Tests/CmdLineParser/run.bat
+++ Tests/CmdLineParser/run.bat
@@ -1,1 +1,1 @@
-test.cpp.exe -a +b -c:d +e:f ( -g:h ( -i ( +j ) k\l +file:m +cfg:test.cfg ) +r ) +s:t -u +v +v +v +w:x +y:z
+test.cpp.exe -a +b -c:d +e:f ( -g:h ( -i ( +j ) k\l ( ) +file:m +cfg:test.cfg ) +r ) +s:t -u +v +v +v +w +y -x:z

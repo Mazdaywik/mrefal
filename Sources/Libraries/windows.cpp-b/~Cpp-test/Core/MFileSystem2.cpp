a4cc2e191ca8b4aa9f8968cfec59cd90c5533455
Маздайщик <a.v.konovalov87@mail.ru>
2009-12-13 02:44:00 +0300

    Commit created from recovered archive 2009-12-13_02-44-00 1-105499 083463038-differencial.rar

---
+++ Sources/Libraries/windows.cpp-b/~Cpp-test/Core/MFileSystem2.cpp
@@ -0,0 +1,143 @@
+// Automatically generated file. Don't edit!
+#include "refalrts.h"
+
+
+//$LABEL Init
+template <typename T>
+struct InitL_ {
+  static const char *name() {
+    return "Init";
+  }
+};
+
+//$LABEL Final
+template <typename T>
+struct FinalL_ {
+  static const char *name() {
+    return "Final";
+  }
+};
+
+refalrts::FnResult CoreP_MFileSystem2P_InitE_(refalrts::Iter arg_begin, refalrts::Iter arg_end) {
+  refalrts::this_is_generated_function();
+  static unsigned s_counter = 0;
+  refalrts::use_counter(s_counter);
+  do {
+    refalrts::Iter bb_0 = arg_begin;
+    refalrts::Iter be_0 = arg_end;
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_right( bb_0, be_0 );
+    // # Init
+    if( ! refalrts::ident_left(  & InitL_<int>::name, bb_0, be_0 ) ) 
+      break;
+    if( ! empty_seq( bb_0, be_0 ) )
+      break;
+
+    refalrts::reset_allocator();
+    refalrts::Iter res = arg_begin;
+    refalrts::use( res );
+    refalrts::splice_to_freelist( arg_begin, arg_end );
+    return refalrts::cSuccess;
+  } while ( 0 );
+
+  do {
+    refalrts::Iter bb_0 = arg_begin;
+    refalrts::Iter be_0 = arg_end;
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_right( bb_0, be_0 );
+    // # Final
+    if( ! refalrts::ident_left(  & FinalL_<int>::name, bb_0, be_0 ) ) 
+      break;
+    if( ! empty_seq( bb_0, be_0 ) )
+      break;
+
+    refalrts::reset_allocator();
+    refalrts::Iter res = arg_begin;
+    refalrts::use( res );
+    refalrts::splice_to_freelist( arg_begin, arg_end );
+    return refalrts::cSuccess;
+  } while ( 0 );
+
+  return refalrts::cRecognitionImpossible;
+}
+
+refalrts::FnResult CoreP_MFileSystem2P_RemoveDir(refalrts::Iter arg_begin, refalrts::Iter arg_end) {
+  refalrts::this_is_generated_function();
+  do {
+    refalrts::Iter bb_0 = arg_begin;
+    refalrts::Iter be_0 = arg_end;
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_right( bb_0, be_0 );
+    refalrts::Iter eFileName_b_1;
+    refalrts::Iter eFileName_e_1;
+    // e.FileName
+    eFileName_b_1 = bb_0;
+    refalrts::use( eFileName_b_1 );
+    eFileName_e_1 = be_0;
+    refalrts::use( eFileName_e_1 );
+
+    refalrts::reset_allocator();
+    refalrts::Iter res = arg_begin;
+    refalrts::use( res );
+    refalrts::splice_to_freelist( arg_begin, arg_end );
+    return refalrts::cSuccess;
+  } while ( 0 );
+
+  return refalrts::cRecognitionImpossible;
+}
+
+refalrts::FnResult CoreP_MFileSystem2P_Remove(refalrts::Iter arg_begin, refalrts::Iter arg_end) {
+  refalrts::this_is_generated_function();
+  do {
+    refalrts::Iter bb_0 = arg_begin;
+    refalrts::Iter be_0 = arg_end;
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_right( bb_0, be_0 );
+    refalrts::Iter eFileName_b_1;
+    refalrts::Iter eFileName_e_1;
+    // e.FileName
+    eFileName_b_1 = bb_0;
+    refalrts::use( eFileName_b_1 );
+    eFileName_e_1 = be_0;
+    refalrts::use( eFileName_e_1 );
+
+    refalrts::reset_allocator();
+    refalrts::Iter res = arg_begin;
+    refalrts::use( res );
+    refalrts::splice_to_freelist( arg_begin, arg_end );
+    return refalrts::cSuccess;
+  } while ( 0 );
+
+  return refalrts::cRecognitionImpossible;
+}
+
+refalrts::FnResult CoreP_MFileSystem2P_MakeDir(refalrts::Iter arg_begin, refalrts::Iter arg_end) {
+  refalrts::this_is_generated_function();
+  do {
+    refalrts::Iter bb_0 = arg_begin;
+    refalrts::Iter be_0 = arg_end;
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_right( bb_0, be_0 );
+    refalrts::Iter eDirName_b_1;
+    refalrts::Iter eDirName_e_1;
+    // e.DirName
+    eDirName_b_1 = bb_0;
+    refalrts::use( eDirName_b_1 );
+    eDirName_e_1 = be_0;
+    refalrts::use( eDirName_e_1 );
+
+    refalrts::reset_allocator();
+    refalrts::Iter res = arg_begin;
+    refalrts::use( res );
+    refalrts::splice_to_freelist( arg_begin, arg_end );
+    return refalrts::cSuccess;
+  } while ( 0 );
+
+  return refalrts::cRecognitionImpossible;
+}
+

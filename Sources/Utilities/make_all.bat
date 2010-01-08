3060ec066db1418a377c0ab6eacf6eb8b1c6a9c0
Маздайщик <a.v.konovalov87@mail.ru>
2010-01-08 20:13:00 +0300

    Commit created from recovered archive 2010-01-08_20-13-00 1-105015 062202078-differencial.rar

--- Sources/Utilities/make_all.bat
+++ Sources/Utilities/make_all.bat
@@ -1,7 +1,7 @@
 @echo off
 set PROGRAMS=Grab_info Text-To-HTML TODO_list Recoder VersionUpdater
 set PROGRAMS=%PROGRAMS% MergeFindResults ErrorDecoder DocConverter
-set PROGRAMS=%PROGRAMS% DocConverter-wp
+set PROGRAMS=%PROGRAMS% DocConverter-wp ClearXLinx
 
 refgo VersionUpdater /verfile:Utilities_version.txt /srcfile:mUtilitiesBanner.mref >nul
 
@@ -9,7 +9,12 @@ if exist _compilation.log del _compilation.log
 
 call compile_mr %PROGRAMS% >> _compilation.log
 
-del *.ref
-rd /s /q ~Defs Info ~ROut ~XLinx
+del *.ref *.tds
+rd /s /q ~Defs Info ~ROut ~XLinx .Info ~Cpp-01 ~SimRef
 
 copy *.rsl ..\..\Bin
+copy *.exe ..\..\Bin
+
+mkdir exe rsl
+move *.exe exe\
+move *.rsl rsl\

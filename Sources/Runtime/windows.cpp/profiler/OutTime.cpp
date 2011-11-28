9402b16c949216d38902427b6b7f982ca318d2b4
Маздайщик <a.v.konovalov87@mail.ru>
2011-11-28 21:28:08 +0000

    Модульный Рефал: началась разработка новых компоновщиков профиля C++/SR, позже они будут подключены к профилю SR

--- Sources/Runtime/windows.cpp/profiler/OutTime.cpp
+++ Sources/Runtime/windows.cpp/profiler/OutTime.cpp
@@ -1,12 +1,12 @@
 #include "OutTime.h"
 
-std::string outtime(double total_seconds)
+std::string outtime(double total_seconds, char *buffer)
 {
-	static char buffer[32];
-	unsigned hours = unsigned(total_seconds) / 60 / 60;
-	unsigned minutes = unsigned(total_seconds) / 60 - 60 * hours;
-	double seconds = total_seconds - 60*60*hours - 60*minutes;
+  unsigned hours = unsigned(total_seconds) / 60 / 60;
+  unsigned minutes = unsigned(total_seconds) / 60 - 60 * hours;
+  double seconds = total_seconds - 60*60*hours - 60*minutes;
 
-	std::sprintf(buffer, "%02u:%02u:%05.2f", hours, minutes, seconds);
-	return buffer;
+  std::sprintf(buffer, "%02u:%02u:%05.2f", hours, minutes, seconds);
+  return buffer;
 }
+
--- Sources/Runtime/windows.cpp/profiler/OutTime.h
+++ Sources/Runtime/windows.cpp/profiler/OutTime.h
@@ -4,6 +4,6 @@
 #include <string>
 #include <cstdio>
 
-std::string outtime(double total_seconds);
+std::string outtime(double total_seconds, char *buffer);
 
 #endif OutTime_H

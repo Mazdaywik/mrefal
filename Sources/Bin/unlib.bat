8c637e73f0b1131a4e1f2a49d37fdc47f96cbd9e
Маздайщик <a.v.konovalov87@mail.ru>
2015-11-06 09:33:09 +0000

    Модульный Рефал: оптимизация и рефакторинг компоновщика back-end’а Рефал 5.

---
+++ Sources/Bin/unlib.bat
@@ -0,0 +1,16 @@
+@type "..\Bin\MRefal.r5-Not linked.txt" | ^
+find /V ":InOut:" | ^
+find /V ":MInOut:" | ^
+find /V ":MOS:" | ^
+find /V ":FileIO:" | ^
+find /V ":MFileIO:" | ^
+find /V ":MStrings:" | ^
+find /V ":MOrder:" | ^
+find /V ":MSelfDiag:" | ^
+find /V ":MFileSystem:" | ^
+find /V ":Math:" | ^
+find /V ":MMath:" | ^
+find /V ":MDosWinRecoder:" | ^
+find /V ":MXXIO:" | ^
+find /V ":MSwapSupport:" | ^
+find /V ":MLambda:" > "..\Bin\MRefal.r5-Not linked.txt"

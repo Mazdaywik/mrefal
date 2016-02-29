5ae60d0752418aca02631829a6694966f5414062
Маздайщик <a.v.konovalov87@mail.ru>
2016-02-29 19:18:55 +0300

    отладочный файл

---
+++ Sources/Compiler/_extract_profile.bat
@@ -0,0 +1,44 @@
+@echo off
+
+set LOG=%1
+if {%LOG%}=={} set LOG=__log.txt
+
+if exist log.tmp erase log.tmp
+
+for /L %%i in (1,1,11) do (
+  echo %%i
+  mingw32-make profile=R5 mode=BUILD
+  call :SCAN_LOG >> log.tmp
+)
+
+sort log.tmp > %LOG%
+erase log.tmp
+
+goto :EOF
+
+:SCAN_LOG
+  set LINEPROC=HEADER
+  for /F "delims=| tokens=1,2" %%L in (_MRefal_profiler.log) do call :INDIRECT "%%L" "%%M"
+goto :EOF
+
+:INDIRECT
+  call :%LINEPROC% "%~1" "%~2"
+goto :EOF
+
+:HEADER
+  echo %~1 | find "Name: FE-MRefal.MLexer.Profiling-NextToken" >NUL
+  if not errorlevel 1 (
+    set LINEPROC=LINE
+  )
+goto :EOF
+
+:LINE
+  echo %~1^^^|%~2 | find "* Refal time"
+  if not errorlevel 1 (
+    set LINEPROC=SKIP
+  )
+goto :EOF
+
+:SKIP
+  ::
+goto :EOF

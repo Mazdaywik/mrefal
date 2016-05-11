3136e6814895034b08bae918f1136ba173562b3b
Маздайщик <a.v.konovalov87@mail.ru>
2016-05-11 17:16:47 +0300

    Исправлена обработка двойных кавычек в back-end’е Рефала-5 (#2)

--- Sources/Compiler/Tests/details.mk
+++ Sources/Compiler/Tests/details.mk
@@ -1,2 +1,4 @@
 Tests/Refal5-double-quote.test: profile=R5
 Tests/Refal5-double-quote.test: TESTFLAGS=+MP:Tests/Refal5-double-quote.MP
+Tests/Refal5-double-quote.test-s: sprofile=R5
+Tests/Refal5-double-quote.test-s: TESTFLAGS=+MP:Tests/Refal5-double-quote.MP

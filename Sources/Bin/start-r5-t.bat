1e268589fa951a9a558a55703295215eadd4f4e0
Маздайщик <a.v.konovalov87@mail.ru>
2018-05-26 16:04:06 +0300

    FIXED: путь к FS-Extent не должен содержать кириллицу

--- Sources/Bin/start-r5-t.bat
+++ Sources/Bin/start-r5-t.bat
@@ -1,5 +1,5 @@
 @echo off
 echo [Profile: Refal 5]
-refgo -c300 -l200 -t "%~dp0mrefal.r5.rsl" ++extent:"%~dp0FS-Extent.cpp.exe" %*
+refgo -c300 -l200 -t "%~dp0mrefal.r5.rsl" ++extent:"..\Bin\FS-Extent.cpp.exe" %*
 if exist "%~dp0*.tds" del "%~dp0*.tds"
 if exist *.obj del *.obj
